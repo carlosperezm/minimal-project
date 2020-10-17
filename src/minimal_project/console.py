24594667681ecac87b04a8e405a10f0e4111da83
Carlos Perez <carlosperezmolano@gmail.com>
2020-10-17 11:30:05 +0200

    Add files to create a basic CLI app

---
+++ src/minimal_project/console.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import click
+
+from . import __version__
+
+
+@click.command()
+@click.version_option(version=__version__)
+def main():
+    """The hypermodern Python project."""
+    click.echo("Hello, world!")
